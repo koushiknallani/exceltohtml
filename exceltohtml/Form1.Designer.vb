32d5cb06fab1279c12f39b49e1c389c3161a3b6b
Koushik Nallani <koushiknallani@gmail.com>
2017-05-11 00:37:29 -0400

    ExcelToHTML

---
+++ exceltohtml/Form1.Designer.vb
@@ -0,0 +1,172 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(6, 19)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(364, 25)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(376, 19)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 25)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Browse"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(376, 24)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 25)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Browse"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.TextBox3)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Times New Roman", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(29, 40)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(457, 101)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "EXCEL Sheet"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(2, 60)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(87, 19)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Sheet Name:"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(95, 59)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 25)
+        Me.TextBox3.TabIndex = 1
+        Me.ToolTip1.SetToolTip(Me.TextBox3, "Enter the sheet name of the worksheet correctly.")
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(6, 24)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(364, 25)
+        Me.TextBox2.TabIndex = 4
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.BackColor = System.Drawing.SystemColors.Control
+        Me.GroupBox2.Controls.Add(Me.TextBox2)
+        Me.GroupBox2.Controls.Add(Me.Button2)
+        Me.GroupBox2.Font = New System.Drawing.Font("Times New Roman", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.Location = New System.Drawing.Point(29, 160)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(457, 62)
+        Me.GroupBox2.TabIndex = 5
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "HTML File"
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(205, 228)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(94, 37)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Convert"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'ToolTip1
+        '
+        Me.ToolTip1.ShowAlways = True
+        Me.ToolTip1.Tag = "sgsdfgfdgd"
+        Me.ToolTip1.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info
+        Me.ToolTip1.ToolTipTitle = "yuagfysgfgsdaf"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(521, 277)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents ToolTip1 As System.Windows.Forms.ToolTip
+
+End Class
---
+++ exceltohtml/Form1.vb
@@ -0,0 +1,125 @@
+﻿
+Imports System.Text
+Imports Microsoft.Office
+Imports Microsoft.Office.Interop
+
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        OpenFileDialog1.Title = "Select EXCEL Sheet"
+        OpenFileDialog1.Filter = "Excel Worksheets|*.xls;*.xlt;*.xlm;*.xlsx|All Files|*.*"
+        OpenFileDialog1.FileName = "Ecxel Sheet"
+        If OpenFileDialog1.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+            TextBox1.Text = OpenFileDialog1.FileName
+
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        OpenFileDialog1.Title = "Select Destination"
+        OpenFileDialog1.Filter = "HTML|*.html|TEXT File|*.txt|All Files|*.*"
+        OpenFileDialog1.FileName = ".html"
+        If OpenFileDialog1.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+            TextBox2.Text = OpenFileDialog1.FileName
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        If TextBox1.Text = "" Or TextBox2.Text = "" Or TextBox3.Text = "" Then
+            MsgBox("Excel Path/Sheet Name/HTML Path is NULL/Blank")
+            Exit Sub
+
+        End If
+        Try
+            Dim builder As New StringBuilder
+            Dim slno, path, name, roll As String
+            Dim output As String = ""
+            'open
+            Dim myexcel As New Excel.Application
+            myexcel.Workbooks.Open(TextBox1.Text)
+
+            'Extract info
+            myexcel.Sheets(TextBox3.Text).activate()
+            myexcel.Range("A2").Activate()
+            'Dim thisrow As New excelrows
+
+            Do
+                If myexcel.ActiveCell.Value > Nothing Or myexcel.ActiveCell.Text > Nothing Then
+                    slno = myexcel.ActiveCell.Value
+                    myexcel.ActiveCell.Offset(0, 1).Activate()
+
+                    path = myexcel.ActiveCell.Value
+                    myexcel.ActiveCell.Offset(0, 1).Activate()
+
+                    name = myexcel.ActiveCell.Value
+                    myexcel.ActiveCell.Offset(0, 1).Activate()
+
+                    roll = myexcel.ActiveCell.Value
+
+                    'excelrowlist.Add(thisrow)
+
+                    builder.Append("<tr>")
+                    builder.AppendLine()
+                    slno = "<td>" + slno + "</td>"
+
+                    builder.Append(slno)
+                    builder.AppendLine()
+                    path = "<td><img src=""" + path + """  width=""57"" height=""55"" /></td>"
+
+                    builder.Append(path)
+                    builder.AppendLine()
+                    name = "<td>" + name + "</td>"
+
+                    builder.Append(name)
+                    builder.AppendLine()
+                    roll = "<td colspan=""2"">" + roll + "</td>"
+
+                    builder.Append(roll)
+                    builder.AppendLine()
+                    builder.Append("</tr>")
+
+                    builder.AppendLine()
+                    Dim s As String = builder.ToString
+
+                    output = s
+                    myexcel.ActiveCell.Offset(1, -3).Activate()
+
+
+                Else
+                    Exit Do
+                End If
+            Loop
+
+            'close
+            myexcel.Workbooks.Close()
+            myexcel = Nothing
+
+            'Copy code to html 
+            ' MsgBox(output)
+            If System.IO.File.Exists(TextBox2.Text) = True Then
+
+                Dim objWriter As New System.IO.StreamWriter(TextBox2.Text, True)
+
+                objWriter.Write(output)
+                objWriter.Close()
+                MsgBox("Conversion Completed")
+
+            Else
+
+                MsgBox("Select Correct Destination File")
+
+            End If
+        Catch ex As ArgumentException
+            MsgBox(ex.Message)
+        Catch em As Exception
+            MsgBox(em.Message)
+        End Try
+
+        
+
+
+    End Sub
+
+    
+End Class
